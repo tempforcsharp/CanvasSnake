d853baa3c90513838d67ad8280842c6523c7a7a7
Alexey Dovgopolyy <andovgopolyy@gmail.com>
2017-11-01 23:18:46 +0200

    Point hash code

--- ConsoleSnake/Point.cs
+++ ConsoleSnake/Point.cs
@@ -49,6 +49,17 @@ namespace ConsoleSnake
         {
             return !(point1 == point2);
         }
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 17;
 
+                hash = hash * 23 + X.GetHashCode();
+                hash = hash * 23 + Y.GetHashCode();
+
+                return hash;
+            }
+        }
     }
 }
